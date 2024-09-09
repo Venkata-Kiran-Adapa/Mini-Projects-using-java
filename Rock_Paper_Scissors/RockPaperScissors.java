fe93c5def3c67cda4b703ffb3384a4f6a6babd47
venkata kiran Adapa <venkatakiranadapa@gmail.com>
2024-09-09 09:26:45 +0530

    commit

--- Rock_Paper_Scissors/RockPaperScissors.java
+++ Rock_Paper_Scissors/RockPaperScissors.java
@@ -12,7 +12,52 @@ public class RockPaperScissors {
         else if (opt==2) rps2Players();
         else System.out.println("Choose from the above 2 options only");
     }
-    public static void rps2Players(){
+
+
+    public static void rpsWithComputer(){
+        Scanner sc=new Scanner(System.in);
+        Random rand=new Random();
+        int p1count=0,p2count=0;
+        String p2="";
+        while(true){
+            System.out.println("choose one from the  below: \n1 for Rock\t2 for Paper\t3 for Scissor");
+            System.out.println("Player Enter: ");
+            int pi1=sc.nextInt();
+            if((0>=pi1 || pi1>=4)) {
+                System.out.println("Enter from above options only");
+                continue;
+            }
+            String p1 = switch (pi1) {
+                case 1 -> "rock";
+                case 2 -> "paper";
+                case 3 -> "scissor";
+                default -> "nothing";
+            };
+            int random=rand.nextInt(3)+1;
+            p2 = switch (random) {
+                case 1 -> "rock";
+                case 2 -> "paper";
+                case 3 -> "scissor";
+                default -> p2;
+            };
+            if((p1.equals("scissor") && p2.equals("rock") )|| (p1.equals("paper") && p2.equals("scissor")) || (p1.equals("rock") && p2.equals("paper")) ){
+                p2count++;
+            }
+            else if((p1.equals("paper") && p2.equals("rock")) || (p1.equals("rock") && p2.equals("scissor")) ||(p1.equals("scissor") && p2.equals("paper"))){
+                p1count++;
+            }
+            if(p1count==3){
+                System.out.println("Player 1 Wins...!");
+                break;
+            }
+            else if(p2count==3) {
+                System.out.println("Computer wins....!");
+                break;
+            }
+        }}
+
+
+public static void rps2Players(){
         Scanner sc=new Scanner(System.in);
         int p1count=0,p2count=0;
         while(true){
@@ -54,46 +99,4 @@ public class RockPaperScissors {
             }
         }
     }
-    public static void rpsWithComputer(){
-        Scanner sc=new Scanner(System.in);
-        Random rand=new Random();
-        int p1count=0,p2count=0;
-        String p2="";
-        while(true){
-            System.out.println("choose one from the  below: \n1 for Rock\t2 for Paper\t3 for Scissor");
-            System.out.println("Player Enter: ");
-            int pi1=sc.nextInt();
-            if((0>=pi1 || pi1>=4)) {
-                System.out.println("Enter from above options only");
-                continue;
-            }
-            String p1 = switch (pi1) {
-                case 1 -> "rock";
-                case 2 -> "paper";
-                case 3 -> "scissor";
-                default -> "nothing";
-            };
-        int random=rand.nextInt(3)+1;
-            p2 = switch (random) {
-                case 1 -> "rock";
-                case 2 -> "paper";
-                case 3 -> "scissor";
-                default -> p2;
-            };
-            if((p1.equals("scissor") && p2.equals("rock") )|| (p1.equals("paper") && p2.equals("scissor")) || (p1.equals("rock") && p2.equals("paper")) ){
-                p2count++;
-            }
-            else if((p1.equals("paper") && p2.equals("rock")) || (p1.equals("rock") && p2.equals("scissor")) ||(p1.equals("scissor") && p2.equals("paper"))){
-                p1count++;
-            }
-            if(p1count==3){
-                System.out.println("Player 1 Wins...!");
-                break;
-            }
-            else if(p2count==3) {
-                System.out.println("Computer wins....!");
-                break;
-            }
-
-    }}
 }
